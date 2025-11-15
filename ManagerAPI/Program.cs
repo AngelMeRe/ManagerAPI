8735c17ed9b5fcac038931e18940fe2316f2348c
AngelMeRe <josemelchoreyes@gmail.com>
2025-11-15 16:25:27 -0600

    Integracion URL de Vercel

--- ManagerAPI/Program.cs
+++ ManagerAPI/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
-                "https://tu-frontend-prod.com"
+                "https://managerfrontend-yibm.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
